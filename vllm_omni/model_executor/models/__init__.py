a874410903031e2c6ac1c0d7c7d78d178b9ee55f
wuli666 <421774554@qq.com>
2026-02-03 17:56:18 +0800

    fix: some question
    Signed-off-by: wuli666 <421774554@qq.com>

--- vllm_omni/model_executor/models/__init__.py
+++ vllm_omni/model_executor/models/__init__.py
@@ -1,6 +1,4 @@
 from .qwen3_omni import Qwen3OmniMoeForConditionalGeneration
 from .registry import OmniModelRegistry  # noqa: F401
 
-__all__ = [
-    "Qwen3OmniMoeForConditionalGeneration",
-]
+__all__ = ["Qwen3OmniMoeForConditionalGeneration"]
